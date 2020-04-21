de77d604c0815189c79e2557f52ad93d761779e0
Josh Harvey <josh@lamassu.is>
2020-04-21 23:01:12 +0200

    latest long-vol

--- yanshuf.py
+++ yanshuf.py
@@ -5,17 +5,22 @@ import data_loader
 from itertools import islice
 from monthdelta import monthmod
 from parsers import load_all
+from math import isnan
 
 algo_stacks = [
-    ('qre', [bt.algos.RunQuarterly(),
-             bt.algos.SelectAll(),
-             bt.algos.WeighEqually(),
-             bt.algos.Rebalance()]
+    ('qr', [bt.algos.RunQuarterly(),
+            bt.algos.SelectAll(),
+            bt.algos.WeighInvVol(),
+            bt.algos.Rebalance()]
      )
 ]
 
 stat_keys = ['max_drawdown', 'monthly_vol', 'best_month', 'best_year', 'worst_month',
              'worst_year', 'monthly_skew', 'monthly_sharpe', 'cagr', 'calmar']
+
+
+def nm(val, base):
+    return min([base, val]) / base
 
 
 def backtest(algo_stack, keys, data):
@@ -36,25 +41,29 @@ def backtest(algo_stack, keys, data):
     months = months_rec[0].months
 
     return_table = res[strategy_name].return_table
-
+    mar_2020 = return_table.at[2020, 'Mar']
+    ytd_2020 = return_table.at[2020, 'YTD']
+    explosivity = mar_2020 / (2*-ss['max_drawdown'])
+    score = explosivity
+    score = ss['cagr']
     extras = pd.DataFrame([pd.Series({
         'correlation': corr,
-        'explosivity': ss['best_month'] / (2*-ss['max_drawdown']),
-        'mar-2020': return_table.at[2020, 'Mar'],
-        '2020': return_table.at[2020, 'YTD'],
+        'explosivity': explosivity,
+        'mar-2020': mar_2020,
+        '2020': ytd_2020,
         '2019': return_table.at[2019, 'YTD'],
         '2018': return_table.at[2018, 'YTD'],
-        'months': months
+        'months': months,
+        'score': score
     }, name=strategy_name)])
 
-    index = ['cagr', 'mar-2020', '2020', '2019', '2018', 'monthly_vol', 'max_drawdown',
-             'explosivity', 'correlation', 'calmar', 'monthly_skew', 'monthly_sharpe',
-             'best_month', 'worst_month', 'best_year', 'worst_year', 'months'
+    index = ['score', 'months', 'correlation', 'cagr', 'explosivity', 'mar-2020', '2020', '2019', '2018', 'monthly_vol', 'max_drawdown',
+             'calmar', 'monthly_skew', 'monthly_sharpe',
+             'best_month', 'worst_month', 'best_year', 'worst_year'
              ]
 
     # re-order columns
-    final = filtered_stats.transpose().join(
-        extras)[index]
+    final = filtered_stats.transpose().join(extras)[index]
 
     return final
 
@@ -77,28 +86,28 @@ def long_vol_score(df):
 
 
 def to_float_fmt(f):
-    return "None" if f == None else f"{f:.3f}"
+    return "None" if isnan(f) else f"{f:.3f}"
 
 
 def to_pct_fmt(f):
-    return "None" if f == None else f"{f:.2%}"
+    return "None" if isnan(f) else f"{f:.2%}"
 
 
 def to_int_fmt(f):
-    return f'{f}'
+    return f'{f:.0f}'
 
 
 def to_html():
     long_vol_groups = data_loader.long_vol_groups()
 
     df = run_all(long_vol_groups)
-    df.sort_values('explosivity', inplace=True, ascending=False)
+    df.sort_values('score', inplace=True, ascending=False)
 
     html = df.style\
         .format(to_pct_fmt)\
-        .format(to_float_fmt,
-                subset=['correlation', 'calmar', 'months', 'explosivity', 'monthly_skew', 'monthly_sharpe'])\
         .format(to_int_fmt, subset=['months'])\
+        .format(to_float_fmt,
+                subset=['score', 'correlation', 'calmar', 'explosivity', 'monthly_skew', 'monthly_sharpe'])\
         .render()
 
     return html
